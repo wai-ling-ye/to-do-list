38dda0b7889c45736ea5bda311b89b9ff07c64c9
Wai Ling Ye <wai.ling.ye@gmail.com>
2018-03-06 21:13:03 -0500

    Partial completion of visual portion of Assignment 5 - DOM Events - Part 1

--- app.js
+++ app.js
@@ -8,17 +8,45 @@ function onReady() {
     event.preventDefault();
 
     let title = newToDoText.value;
-
     let newLi = document.createElement('li');
+    let primaryContent = document.createElement('span');
 
+    // The following lets are for the checkbox html
+    let secondaryContent = document.createElement('span');
+    let label = document.createElement('label');
     let checkbox = document.createElement('input');
 
+    newLi.className = "mdl-list__item";
+    // Primary Content - to-do text
+    primaryContent.className = "mdl-list__item-primary-content";
+    primaryContent.textContent = title;
+
+    // Secondary Content - checkbox
+    secondaryContent.className = "mdl-list__item-secondary-action";
+
+    label.className = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
+    label.htmlFor = "list-checkbox";
+
     checkbox.type = "checkbox";
+    checkbox.className = "mdl-checkbox__input";
+    checkbox.id = "chkbox";
 
-    newLi.textContent = title;
+    // Upgrade all MDL affected elements
+    componentHandler.upgradeElement(primaryContent);
+    componentHandler.upgradeElement(checkbox);
 
-    newLi.appendChild(checkbox);
+    // Creating the checkbox space
+    label.appendChild(checkbox);
+    componentHandler.upgradeElement(label);
+    secondaryContent.appendChild(label);
+    componentHandler.upgradeElement(secondaryContent);
 
+    // Creating the list item
+    newLi.appendChild(primaryContent);
+    newLi.appendChild(secondaryContent);
+    componentHandler.upgradeElement(newLi);
+
+    // Adding the new item to the list
     toDoList.appendChild(newLi);
 
     newToDoText.value = '';
@@ -28,7 +56,7 @@ function onReady() {
     event.preventDefault();
 
     for (i=toDoList.children.length-1; i>=0; i--) {
-      if (toDoList.children[i].lastChild.checked == true) {
+      if (toDoList.children[i].lastChild.firstChild.firstChild.checked == true) {
         toDoList.removeChild(toDoList.children[i])
       }
     }
